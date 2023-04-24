0c53f836a445994682a124145e21e603426dff2a
vkasyan76 <vkasyan@gmx.de>
2023-04-24 06:37:58 +0200

    Creating a Table in the DB / Testing Data Loaad

--- server/db.js
+++ server/db.js
@@ -10,6 +10,21 @@ const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const DATABASE = process.env.DATABASE;
 
+async function create({ singleStoreConnection, comment }) {
+  const [results] = await singleStoreConnection.execute(
+    "INSERT INTO comments (commentid, commenter, comment, gpt, flag, respond) VALUES (?, ?, ?, ?, ?, ?)",
+    [
+      comment.commentid,
+      comment.commenter,
+      comment.comment,
+      comment.gpt,
+      comment.flag,
+      comment.respond,
+    ]
+  );
+  return results.insertId;
+}
+
 // main is run at the end
 async function main() {
   let singleStoreConnection;
@@ -22,6 +37,20 @@ async function main() {
     });
 
     console.log("You have successfully connected to SingleStore.");
+
+    // call the create() function inside the main() function as:
+    const id = await create({
+      singleStoreConnection,
+      comment: {
+        commentid: 1,
+        commenter: "Adrian Twarog",
+        comment: "Why did the database go on a diet? It had too many tables!",
+        gpt: "",
+        flag: 0,
+        respond: 0,
+      },
+    });
+    console.log(`Inserted row id is: ${id}`);
   } catch (err) {
     console.error("ERROR", err);
     process.exit(1);
