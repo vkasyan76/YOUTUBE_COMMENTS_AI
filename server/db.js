1844f64d1704285665c4a4b78c1b4c897a18a352
vkasyan76 <vkasyan@gmx.de>
2023-04-27 15:49:22 +0200

    Reading und Updating DB with OpenAI

--- server/db.js
+++ server/db.js
@@ -69,6 +69,13 @@ async function main() {
 
 // copy main and modify: create update db function
 
+// Read the data:
+
+async function readN({ singleStoreConnection }) {
+  const [rows] = await singleStoreConnection.execute("SELECT  * FROM comments");
+  return rows;
+}
+
 async function updateDatabaseUsingGPT() {
   let singleStoreConnection;
   try {
@@ -80,6 +87,43 @@ async function updateDatabaseUsingGPT() {
     });
 
     console.log("You have successfully connected to SingleStore.");
+
+    const comments = await readN({ singleStoreConnection });
+
+    console.log(comments);
+
+    for (var i = 0; i < comments.length; i++) {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt:
+          `The following AI tool helps youtubers identify if a comment can should be replied to or not. Questions and or asking for advice are good examples of when a reply is needed.\n\n` +
+          // Context Example 1
+          `User: John Smith\n` +
+          `Comment: That was a great video, thanks!\n` +
+          `Should Reply: No\n\n` +
+          // Context Example 2
+          `User: Sue Mary\n` +
+          `Comment: I'm stuck on step four, how do I do it?` +
+          `Should Reply: Yes\n\n` +
+          // Actual use case
+          `User: ${comments[0].commenter}\n` +
+          `Comment: ${comments[i].comment}\n` +
+          `Should Reply:`,
+        stop: ["\n", "User:", "Comment:", "Should Reply:"],
+        max_tokens: 7,
+        temperature: 0,
+      });
+
+      console.log(response.data.choices[0].text);
+
+      if (response.data.choices[0].text.trim() == "Yes") {
+        await singleStoreConnection.execute(
+          `UPDATE comments SET respond = 1 WHERE id = ${comments[i].id}`
+        );
+
+        console.log("database updated");
+      }
+    }
   } catch (err) {
     console.error("ERROR", err);
     process.exit(1);
@@ -89,3 +133,31 @@ async function updateDatabaseUsingGPT() {
     }
   }
 }
+
+// updateDatabaseUsingGPT();
+
+async function readDatabase() {
+  let singleStoreConnection;
+  try {
+    singleStoreConnection = await mysql.createConnection({
+      host: HOST,
+      user: USER,
+      password: PASSWORD,
+      database: DATABASE,
+    });
+    console.log("You have successfully connected to SingleStore.");
+
+    const comments = await readN({ singleStoreConnection });
+
+    console.log(comments);
+  } catch (err) {
+    console.error("ERROR", err);
+    process.exit(1);
+  } finally {
+    if (singleStoreConnection) {
+      await singleStoreConnection.end();
+    }
+  }
+}
+
+readDatabase();
